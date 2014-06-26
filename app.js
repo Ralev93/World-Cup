9049b74ca9f796f645b288d03f466b676d051b0b
Hristo Ralev <hbr@abv.bg>
2014-06-26 15:14:25 +0300

    update

--- app.js
+++ app.js
@@ -2,51 +2,20 @@
 
 $(document).ready(function() {
 
-	var matches = [];
-
-	var source   = $("#entry-template").html();
-	var template = Handlebars.compile(source);
-
-
 	$.getJSON( "http://worldcup.sfg.io/matches/today", function( data ) {
-	  var items = [];
-	  $.each( data, function( key, val ) {
-	    items.push(val);
-	  });
-	 // console.log(items);
-	 matches = items;
-	});
-	/*
-	var data = {
-	    users: [ {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "Front End Technical Lead",
-	        twitter: "gazraa"
-	    }, {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "Photographer",
-	        twitter: "photobasics"
-	    }, {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "LEGO Geek",
-	        twitter: "minifigures"
-	    } ]
-	};
 
-	Handlebars.registerHelper('fullName', function(person) {
-	  return person.firstName + " " + person.lastName;
+		var source   = $("#entry-template").html();
+		var template = Handlebars.compile(source);
+
+		var context = {'match': data, num: "0"};
+		var html    = template(context);
+
+		console.log(html);
+		$('body').append(html);
+
 	});
 
-	*/
-	console.log(matches);
-	$('#wtf').append(template(matches));
 });
+
+
+
