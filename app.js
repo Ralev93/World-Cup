1f530452decca7ab0c43b7f0711254d46e72f55c
Marianna Vladimirova <mvladimirova@localhost.localdomain>
2014-06-26 08:20:58 -0400

    Add basic jumbotron views

--- app.js
+++ app.js
@@ -2,51 +2,14 @@
 
 $(document).ready(function() {
 
-	var matches = [];
+    $.getJSON( "http://worldcup.sfg.io/matches/today", function( data ) {
+    	var source   = $("#entry-template").html();
+    	var template = Handlebars.compile(source);
+        var content = {'match': data};
+        var html = template(content);
+        console.log(html);
+    	$('body').append(html);
+    });
+    
 
-	var source   = $("#entry-template").html();
-	var template = Handlebars.compile(source);
-
-
-	$.getJSON( "http://worldcup.sfg.io/matches/today", function( data ) {
-	  var items = [];
-	  $.each( data, function( key, val ) {
-	    items.push(val);
-	  });
-	 // console.log(items);
-	 matches = items;
-	});
-	/*
-	var data = {
-	    users: [ {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "Front End Technical Lead",
-	        twitter: "gazraa"
-	    }, {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "Photographer",
-	        twitter: "photobasics"
-	    }, {
-	        person: {
-	            firstName: "Garry",
-	            lastName: "Finch"
-	        },
-	        jobTitle: "LEGO Geek",
-	        twitter: "minifigures"
-	    } ]
-	};
-
-	Handlebars.registerHelper('fullName', function(person) {
-	  return person.firstName + " " + person.lastName;
-	});
-
-	*/
-	console.log(matches);
-	$('#wtf').append(template(matches));
 });
