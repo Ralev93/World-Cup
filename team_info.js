23769223c5dcc47997f58f2e0700887d5d530adc
Marianna Vladimirova <mvladimirova@localhost.localdomain>
2014-07-01 09:12:07 -0400

    Enter some changes
    -needs debugging

--- team_info.js
+++ team_info.js
@@ -2,15 +2,25 @@
 
 $(document).ready(function(){
 	$.getJSON("http://worldcup.sfg.io/teams/", function (teams) {
-		console.log(teams);
-		var source   = $("#entry-template").html();
-		var template = Handlebars.compile(source);
+		$.getJSON("http://worldcup.sfg.io/teams/results", function (team_results){
+			
+			var source   = $("#entry-template").html();
+			var template = Handlebars.compile(source);
 
-		var context = {'team':teams};
-		var html = template(context);
+			var context = {'team':teams};
+			var html = template(context);
 
-		$("#dropdown-li").append(html);
 
+			var context_1 = {'team_results':team_results};
+			var html_1 = template(context_1);
+
+			$("#dropdown-li").append(html);
+			$("#country_tables").append(html_1);
+
+		});
 	});
 
+	$(document).on("click", ".country", function() {
+		console.log($(this).data("country"))
+	});
 });
